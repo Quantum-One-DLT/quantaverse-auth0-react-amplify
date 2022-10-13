87e282cf584c4f44a3ab50c75e87eb29041fd294
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-12 20:18:35 -0400

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,16 +4,15 @@ const logo = "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.c
 
 const Hero = () => (
   <div className="text-center hero">
-    <img className="mb-3 app-logo" src={logo} alt="quantumOne-logo" width="120" />
-    <h1 className="mb-4">Quantum One is...</h1>
+    <img className="mb-3 app-logo" src={logo} alt="quantumOne-logo" width="72" />
+    <h1 className="mb-4">Create your QuantaVerse account and</h1>
     <p className="lead">
-      ...not Web2, Web3, Web5, nor X, it is the {" "}
+      and become eligible for Quantum One DAO's airdrop campaign!{" "}
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://quantumone.network"
-      >
-        QuantaVerse 
+      > 
       </a>
     </p>
   </div>
