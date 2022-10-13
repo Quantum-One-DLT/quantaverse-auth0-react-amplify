cd22cd512c6542cdac8f42452d844175ba9e8488
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-13 14:30:09 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,9 +4,9 @@ const Footer = () => (
   <footer className="bg-light p-3 text-center">
     <div className="logo" />
     <p>
-      Sample project provided by{" "}
-      <a target="_blank" rel="noopener noreferrer" href="https://auth0.com">
-        Auth0
+      Quantaverse ID is a registered trademark of{" "}
+      <a target="_blank" rel="noopener noreferrer" href="https://quantumone.network">
+        Quantum One DAO LLC
       </a>
     </p>
   </footer>
