c2e8223dab0c153cfe162b967197e814f5386cf8
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-13 19:22:52 -0400

    app logo

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,15 +1,12 @@
 import React from "react";
 
 import MainNav from "./main-nav";
-import logo from "./logo.svg";
-import Logo from "./logo";
 
 const NavBar = () => {
   return (
     <nav className="navbar" style="background-color: #03fff6;">
       <div className="container-fluid mb-3">
-        <a clasName = "navbar-brand" href="#">
-          <Logo />
+        <a className = "app-logo" href="#">
           </a>   
        <MainNav />
          </div>
