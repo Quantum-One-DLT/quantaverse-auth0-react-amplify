7107dfa75b63b83829bba383e5360cfe13a3ab4e
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-13 15:29:37 -0400

    Create Logo.js

---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+
+const logoImg = {logo};
+
+const Logo = () => (
+
+  <div className="logo">
+
+    <img src={logoImg} alt="quantumone-logo" />
+
+  </div>
+
+);
+
+export default Logo;
