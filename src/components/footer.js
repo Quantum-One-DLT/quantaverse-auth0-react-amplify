b30b0bca83589ba52ebfdeab86244d6ab1f135f3
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-13 19:15:54 -0400

    footer logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Logo from "./logo";
 
 const Footer = () => (
   <footer className="bg-light p-3 text-center">
-    <div className="logo" />
+    <div>
+    <Logo/>
     <p>
       Quantaverse ID is a registered trademark of{" "}
       <a target="_blank" rel="noopener noreferrer" href="https://quantumone.network">
         Quantum One DAO LLC
       </a>
     </p>
+    </div>
   </footer>
 );
 
