1a480fe887ff1bffc669bd5e436857708be29f9b
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-13 15:23:56 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
+import Logo from "./logo";
 import HomeContent from "./home-content";
 import Footer from "./footer";
 import Hero from "./hero";
 import Loading from "./loading";
 import NavBar from "./nav-bar";
 
-export { HomeContent, Footer, Hero, Loading, NavBar };
+export { Logo, HomeContent, Footer, Hero, Loading, NavBar };
