3344792578619d1a842a83811e413e6d8acd4d5c
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-14 13:47:46 -0400

    cdn

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const logo = "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd3eba284-1022-41ef-a3bc-9c218ed8c3ad%2Fimg_202263155825.png?table=block&id=7df3771e-2418-4020-b3ff-bf808d140ae6&spaceId=1a5e50b6-2a76-42f5-8c0a-38a14e587b94&width=72&userId=538a2d1e-ea3a-494d-8533-d1924dbf1cab&cache=v2";
+const logo = "https://quantumone.b-cdn.net/images/logo.svg";
 
 const Hero = () => (
   <div className="text-center hero">
--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -3,7 +3,7 @@ import MainNav from "./main-nav";
 
 const NavBar = () => {
   return (
-   <div className="nav-container mb3">
+   <div className="nav-container mb-3">
     <nav className="navbar navbar-expand-lg navbar-dark bg-light">
      <div className="container">
       <a className="navbar-brand" href="%PUBLIC_URL%/logo.svg">Quantum One DAO</a>
