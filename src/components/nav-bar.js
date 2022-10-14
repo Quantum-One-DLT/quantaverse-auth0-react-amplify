a9b44c1bad8ae32cadf28ca55e7c9d5899a057fb
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-13 21:27:49 -0400

    navbar, logo b-c

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,13 +1,14 @@
 import React from "react";
-
 import MainNav from "./main-nav";
 
 const NavBar = () => {
   return (
-    <nav className="navbar" style="background-color: #03fff6;">
+    <nav className="navbar" style={{"background-color": "#03fff6"}}>
       <div className="container-fluid mb-3">
-        <a className = "app-logo" href="#">
-          </a>   
+        <a class="navbar-brand" href="https://quantaverseid.io">
+          <img src=".src/components/logo.svg" width="118" height="30" class="d-inline-block align-top" alt=""/>
+          </a>
+    QuantaVerse ID
        <MainNav />
          </div>
       </nav>
