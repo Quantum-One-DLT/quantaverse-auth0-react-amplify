5c38e333f5df28b594bb8f4a16483d925666a976
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-14 12:02:57 -0400

    Update nav-bar.js

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -6,13 +6,13 @@ const NavBar = () => {
    <div className="nav-container mb3">
     <nav className="navbar navbar-custom navbar-expand-lg navbar-dark bg-light">
      <div className="container-fluid">
-      <a className="navbar-brand" href="#">Quantum One DAOr</a></div>
+      <a className="navbar-brand" href="#">Quantum One DAOr</a>
       <div class="container-fluid">
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
       </button>
-      </div>
      <MainNav/>
+     </div>
    </nav>
   </div>
   );
