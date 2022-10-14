8fe3e55f600b886e0b8b8dd734a7315dcb2cd757
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-14 01:28:57 -0400

    hr

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -24,7 +24,7 @@ const NavBar = () => {
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
+            <li><hr class="dropdown-divider"></hr></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
