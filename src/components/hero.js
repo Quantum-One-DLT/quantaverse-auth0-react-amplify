0af052bd71acc84e740a2e4a4714e745d0a72892
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-14 11:40:21 -0400

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,14 +5,14 @@ const logo = "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.c
 const Hero = () => (
   <div className="text-center hero">
     <img className="mb-3 app-logo" src={logo} alt="quantumOne-logo" width="96" />
-    <h1 className="mb-4"> Humanity's optimal digital paradigm airrop </h1>
+    <h1 className="mb-4"> Humanity's optimal digital trust paradigm airdrop </h1>
     <p className="lead">
-       The 1st 3500 users who verify their ID or prove prior KYC will become one of the earliest members of {" "}
+       The 1st 3500 users who verify their ID or prove prior KYC will become one of the earliest members of Quantum One DAO. {" "}
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://app.fractal.id/authorize?client_id=QHAVw9SmY3f0dkaNB8GLnL8SWlRf_Qk8708qAKHJgkA&redirect_uri=https%3A%2F%2Fquantaverseid.io%2Fcallback&response_type=code&scope=contact%3Aread%20verification.basic%3Aread%20verification.basic.details%3Aread%20verification.liveness%3Aread%20verification.liveness.details%3Aread"
-      >Verify ID
+      >Verify ID now
       </a>
     </p>
   </div>
