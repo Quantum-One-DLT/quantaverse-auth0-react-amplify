4d25f05b5cb6efff5d45de9f929bda450b01aaf5
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-15 12:01:43 -0400

    secondary

--- src/components/loginButton.js
+++ src/components/loginButton.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+  
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <button
-      className="btn btn-primary btn-block"
+      className="btn btn-secondary btn-block"
       onClick={() => loginWithRedirect()}
     >
       Log In
