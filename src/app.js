463319b9987d7446f3653d4dbccba46e9f6d3b78
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-15 12:33:30 -0400

    Up

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import "./app.css";
 
 const App = () => {
   return (
-    <div id="app" className="d-flex flex-column h-100" style={{background: "linear-gradient(to right top, #03fff6, #00d0d9, #1fa2b5, #2f778b, #2f4f5e, #2e4c5a, #2c4856, #2b4552, #2a6576, #1e8797, #00aab2, #00cfc8)"}}>
+    <div id="app" className="d-flex flex-column h-100" style={{background: "linear-gradient(to right top, #1e2022, #2d424f, #316a7c, #2794a8, #06c1ce, #04cede, #02dcee, #00e9ff, #00d7ff, #00c3ff, #00aeff, #0096ff)"}}>
       <NavBar />
       <div className="container flex-grow-1">
         <div className="mt-5">
