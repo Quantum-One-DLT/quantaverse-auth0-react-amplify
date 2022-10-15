ac3b2d5dcd46d32cc75b6cd0e44c11eda2fb0532
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-15 04:54:51 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,9 +6,17 @@ import { Home, Profile, ExternalApi } from "./views";
 
 import "./app.css";
 
+const styles={{  
+  backgroundImage: "url(" + "https://quantumone.b-cdn.net/Untitled%20Design_2-6.jpg" + ")",
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat'
+}}
+
 const App = () => {
   return (
-    <div id="app" className="d-flex flex-column h-100" style={{background: "linear-gradient(to right top, #03fff6, #00d0d9, #1fa2b5, #2f778b, #2f4f5e, #2e4c5a, #2c4856, #2b4552, #2a6576, #1e8797, #00aab2, #00cfc8)"}}>
+    <div id="app" className="d-flex flex-column h-100" style={{styles}}>
+      <NavBar />}}>
       <NavBar />
       <div className="container flex-grow-1">
         <div className="mt-5">
