28e874f5b437e6f201eb039ab73a601744172e25
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-11-03 19:29:45 -0400

    fix

--- src/components/connectWallet.js
+++ src/components/connectWallet.js
@@ -1,9 +1,10 @@
 import { useConnectWallet } from '@web3-onboard/react'
 import { ethers } from 'ethers'
+import { useState } from 'react'
 
 export default function ConnectWallet() {
   const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
-  const [ethersProvider, setProvider] = useState<ethers.providers.Web3Provider | null>()
+  const [ethersProvider, setProvider ] = useState()
 
   useEffect(() => {
     // If the wallet has a provider than the wallet is connected
