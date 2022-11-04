03eb425aed8739a872f284fbaac525ce1ee3db50
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-11-04 13:05:11 -0400

    Create craco.config.js

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+module.exports = { 
+   reactScriptsVersion: "react-scripts" /* (default value) */, 
+  
+   webpack: { 
+     alias: { 
+       "magic-sdk": path.resolve( 
+         __dirname, 
+         "node_modules/magic-sdk/dist/cjs/index.js" 
+       ), 
+     }, 
+   }, 
+ };
