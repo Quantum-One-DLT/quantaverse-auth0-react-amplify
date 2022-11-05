73c1ab9d88be9641519bd3acfa1e10e20675b738
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-11-05 16:31:59 -0400

    rm

--- src/app.js
+++ src/app.js
@@ -32,12 +32,9 @@ const theme = createTheme({
       main: "#1e2022",
       contrastText: "#ffffff",
     }, 
-  button: { 
-     width:  72px,
-     height: 28px,
-     color:  blue
+  
   },
-},
+
 });
 
 const App = () => {
