08672d3fa3ce8e7cec6ed63622c2d787c430d1b5
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-11-06 15:02:37 -0500

    fix import

--- src/network.js
+++ src/network.js
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import { Contract, ethers } from "ethers";
 import { createTheme, ThemeProvider } from "@material-ui/core";
 import { SnackbarProvider } from "notistack";
-import { NavBar } from "./components/nav-bar";
+import { NavBar } from "./components";
 import ConnectWalletPage from "./views/connectWalletPage";
 import {
   getAccount,
