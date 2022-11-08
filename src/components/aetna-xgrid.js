67cc8d31b208dc28b7ce2968ddfcf29b7470a962
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-11-08 00:01:02 -0500

    update

--- src/components/aetna-xgrid.js
+++ src/components/aetna-xgrid.js
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { XGrid } from '@material-ui/x-grid';
 import {LicenseInfo} from '@material-ui/x-grid';
 import { useAuth0 } from "@auth0/auth0-react";
@@ -29,15 +29,15 @@ const propTypes = {
         })
       })
     )
-  }
+};
   
-  const columns = [
-    { field: 'plan_name', headerName: 'PLAN NAME', width: 170 },
-    { field: 'plan_id_type', headerName: 'PLAN ID TYPE', width: 170 },
-    { field: 'plan_id', headerName: 'PLAN ID', width: 170 },
-    { field: 'plan_market_type', headerName: 'MARKET', width: 170 },
-    { field: 'in_network_files', headerName: 'IN NETWORK FILES', width: 170},
-    { field: 'allowed_amount_files', headerName: 'ALLOWED AMOUNT FILES', width: 170}
+const columns = [
+  { field: 'plan_name', headerName: 'PLAN NAME', width: 170 },
+  { field: 'plan_id_type', headerName: 'PLAN ID TYPE', width: 170 },
+  { field: 'plan_id', headerName: 'PLAN ID', width: 170 },
+  { field: 'plan_market_type', headerName: 'MARKET', width: 170 },
+  { field: 'in_network_files', headerName: 'IN NETWORK FILES', width: 170},
+  { field: 'allowed_amount_files', headerName: 'ALLOWED AMOUNT FILES', width: 170}
   ];
 
   const rows = [
@@ -49,44 +49,42 @@ const propTypes = {
     {plan_name: 'MIDDLESEX HEALTH SYSTEM, INC.Open Access Aetna SelectLT', plan_id_type: 'EIN', plan_id: '060646718', plan_market_type: 'Group', in_network_files: 'https://mrf.healthsparq.com/aetnacvs-egress.nophi.kyruushsq.com/prd/mrf/AETNACVS_I/ALICSI/2022-10-05/inNetworkRates/2022-10-05_773a4563-879c-4bd7-babf-e668bf072774_Aetna-Life-Insurance-Company.json.gz', allowed_amount_file: 'https://mrf.healthsparq.com/aetnacvs-egress.nophi.kyruushsq.com/prd/mrf/AETNACVS_I/ALICSI/2022-10-05/allowedAmounts/2022-10-05_773a4563-879c-4bd7-babf-e668bf072774_Aetna-Life-Insurance-Company.json.gz'}
   ];
 
-  export default function AetnaXGrid() {
+export default function AetnaXGrid() {
 
-   const {
-    isAuthenticated
-  } = useAuth0();
-
-    
-    const [width,setWidth]=React.useState(window.innerWidth);
-    const [height,setHeight]=React.useState(window.innerHeight);
-    const updateWidthAndHeight= () => {
+  const { isAuthenticated } = useAuth0();
+  const [width,setWidth]=React.useState(window.innerWidth);
+  const [height,setHeight]=React.useState(window.innerHeight);
+  const updateWidthAndHeight= () => {
     setWidth(window.innerWidth);
     setHeight(window.innerHeight);
-    };
+  };
 
-    React.useEffect(() => {
+  React.useEffect(() => {
     window.addEventListener("resize",updateWidthAndHeight);
     return () => window.removeEventListener("resize",updateWidthAndHeight);
-    });
+  });
 
     return (
-        {isAuthenticated && (
-         <div style={{ height: 500, width: '80%' }}>
+            <div>
+            {isAuthenticated && (
+            <div style={{ height: 500, width: '80%' }}>
          
-          <h4>HealthONE incentivized healthcare platform</h4>
+            <h4>HealthONE incentivized healthcare platform</h4>
             <p> HealthONE rewards consumers, providers, and insurers for making lifestyle and business decisions that result in a significant reduction in the cost of care and better quality of life. 
                 HealthONE includes future proof post quantum cryptography, mobile remote patient and machine management, and composable compliance that ensures you're spending time with those that matter most, not paperwork.</p> 
-          <p>Download at your own discretion - files sizes greater than 30GB are common. We're in the process of making the data easy to read and analyze for the public - not just plan members.</p>
-          <XGrid rows={rows} columns={columns} pageSize={5} 
-          />
-        </div>
-       )}
-       {!isAuthenticated && (
-        <div>
-         <p>Please login to view this page.</p>
-        </div>
-       )}
-    );
-  }
+            <p>Download at your own discretion - files sizes greater than 30GB are common. We're in the process of making the data easy to read and analyze for the public - not just plan members.</p>
+            <XGrid rows={rows} columns={columns} pageSize={5} />
+            </div>
+            )}
+            {!isAuthenticated && (
+             <div>
+              <p>Please login to view this page.</p>
+             </div>
+            )}
+      
+            </div>
+          )
+  };
 
 LicenseInfo.setLicenseKey(
   '19494f35107113d0c813dfab75b058b4T1JERVI6MjQzODgsRVhQSVJZPTE2NTE1MDQzNDkwMDAsS0VZVkVSU0lPTj0x'
