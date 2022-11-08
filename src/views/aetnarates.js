50d9dca097aa0423d68a6fefcfaefcc011fc7604
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-11-08 02:23:58 -0500

    Create aetnarates.js

---
+++ src/views/aetnarates.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import { AetnaXGrid } from "../components";
+
+const AetnaRates = () => (
+    <Fragment>
+        <div>
+         <AetnaXGrid />
+        </div>
+    </Fragment>
+);
+
+export default AetnaRates;
