6c3cca924472ea50a004878abea41d2f978c56be
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-11-08 17:15:21 -0500

    Update aetna-xgrid.js

--- src/components/aetna-xgrid.js
+++ src/components/aetna-xgrid.js
@@ -70,9 +70,9 @@ export default function AetnaXGrid() {
                   }}
               />
           </div>
-   };
 
- return content;
+  return content;
+};
 
 LicenseInfo.setLicenseKey(
   '19494f35107113d0c813dfab75b058b4T1JERVI6MjQzODgsRVhQSVJZPTE2NTE1MDQzNDkwMDAsS0VZVkVSU0lPTj0x'
