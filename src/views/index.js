2c833db0e04cc97314f703eb2a896b96c7464326
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-11-08 02:25:23 -0500

    Update index.js

--- src/views/index.js
+++ src/views/index.js
@@ -3,5 +3,6 @@ import Home from "./home";
 import Profile from "./profile";
 import Flow from "./flow";
 import Privacy from "./privacy";
+import AetnaRates from "./aetnarates";
 
-export { ExternalApi, Home, Profile, Flow, Privacy };
+export { ExternalApi, Home, Profile, Flow, Privacy, AetnaRates };
