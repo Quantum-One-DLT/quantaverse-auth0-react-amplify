e75698d1c1d14ccd8aa0c0ebe6cabde359d0971c
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-11-21 18:38:59 -0500

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ const Logo = require("../assets/quantumone-home.jpg");
 const Hero = () => (
   <div className="text-center hero">
     <h1 className="mb-4"> QuantaVerse AirDrop </h1>
-    <p className="lead">
+    <h6 className="lead">
        The 1st 3500 users who complete our ID/KYC verification process will receive a random amount of our testnet ERC-20 token and, thus, become one of the earliest members of Quantum One DAO - creators of the QuantaVerse and HealthONE. Once complete with verification, create or link your account using the login button found within the menu at the top right of the page. And check out how we're making it easy for everyone to access public insurance and provider rate data at quantaverseid.io/aetna-sample which contains a sample of Aetna's negotiated rates. As time progresses, we will continue to accumlumate a compendium of rate data from all providers, hospitals, and insurance companies and incorporate into our incentivized healthcare platform - HealthOne. More details will be provided once we launch our new website in the next few days at quantumone.network. To get started, {" "}
       <a
         target="_blank"
@@ -13,7 +13,7 @@ const Hero = () => (
         href="https://app.fractal.id/authorize?client_id=QHAVw9SmY3f0dkaNB8GLnL8SWlRf_Qk8708qAKHJgkA&redirect_uri=https%3A%2F%2Fquantaverseid.io%2Fcallback&response_type=code&scope=contact%3Aread%20verification.basic%3Aread%20verification.basic.details%3Aread%20verification.liveness%3Aread%20verification.liveness.details%3Aread"
       >Verify ID | Prove Prior KYC.
      </a> 
-    </p>
+    </h6>
     <figure className='figure'>
       <img width="100%"
         src={Logo}
