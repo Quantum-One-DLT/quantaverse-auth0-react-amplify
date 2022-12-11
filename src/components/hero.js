aa12365d11fb2809e3cf057a13394c1c5d754e62
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-12-10 21:18:49 -0500

    update

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,20 +5,24 @@ const Logo = require("../assets/tokenomics.jpg");
 const Hero = () => (
 
   <div className="text-center hero">
-    <h2 className="mb-4">We're AirDropping the 1st 3500 new Quantum One DAO users who verify their ID!</h2>
+    <h2 className="mb-4">Verify your ID, get Quantum One DAO's testnet ERC-20 token!</h2>
     <h5 className="text-justified">
-       Join the omni-compliant, post quantum secure, identity-centric DAO who's eliminating blockchain investment risk and disrupting the healthcare market with 
-       it's incentivized POS HealthONE platform. To get started {" "}
+       We're dropping a total of 250 DA-FI tokens to the the 1st 3500 who verify their ID and create a Quantum One account. To get started {" "}
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://app.fractal.id/authorize?client_id=QHAVw9SmY3f0dkaNB8GLnL8SWlRf_Qk8708qAKHJgkA&redirect_uri=https%3A%2F%2Fapp.quantumone.network%2Fcallback&response_type=code&scope=contact%3Aread%20verification.basic%3Aread%20verification.basic.details%3Aread%20verification.liveness%3Aread%20verification.liveness.details%3Aread"
-      >Verify ID | Prove Prior KYC.
+      >Verify ID | Prove Prior KYC
      </a> 
     </h5>
-    <h5 className="text-justified">Be sure to create a Quantum One account upon successful ID verification. Contact us via support message with any questions</h5>
+    <h5 className="text-justified">Contact support below with any questions. Return to Quantum One DAO {" "} </h5>
+    <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://quantumone.network"
+      > homepage.</a>
     <figure className='figure'>
-      <img width="100%"
+      <img width="50%"
         src={Logo}
         className="figure-img img-fluid rounded shadow-3 mb-3"
         alt="quantumone-tokenomics-slide"
