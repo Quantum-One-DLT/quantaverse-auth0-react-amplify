67a3896955adfff5f63d65a7b2e891414eef5b96
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-12-29 13:59:43 -0500

    update

--- src/components/loginButton.js
+++ src/components/loginButton.js
@@ -6,7 +6,7 @@ const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <button
-      className="btn btn-secondary btn-md"
+      className="btn btn-md"
       onClick={() => loginWithRedirect()}
     >
       Sign Up | Log In
