efce56c9d566217efed9d8ee0c32b96d7ec800cd
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-12-28 19:25:58 -0500

    update

--- src/components/aetna-xgrid.js
+++ src/components/aetna-xgrid.js
@@ -8,7 +8,7 @@ import { useState } from "react";
 import LoginButton from './loginButton';
 import LogoutButton from './logoutButton';
 import { useAuth0 } from "@auth0/auth0-react";
-import Accordion from '@mui/material/Accordion';
+import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
