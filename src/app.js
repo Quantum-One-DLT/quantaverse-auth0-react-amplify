422da9e6f21234dc929c24b1d0e5a1a66e4a3e37
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-12-31 17:56:06 -0500

    update

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ import "./app.css";
 
 const App = () => {
   return (
-    <div id="app" className="d-flex flex-column vh-100 gradlite">
+    <div id="app" className="d-flex flex-column gradlite">
       <NavBar />
-      <div className="container flex-grow-1 gradlite">
+      <div className="container flex-grow-1">
         <div className="mt-5">
           <Switch>
             <Route path="/" exact component={AetnaXGrid} />
