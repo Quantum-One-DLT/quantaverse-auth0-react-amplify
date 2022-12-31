c3a2e2079bf3fa44ccf8ab5352d85ffe78e67e42
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-12-31 13:22:24 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import "./app.css";
 
 const App = () => {
   return (
-    <div id="app" className="d-flex flex-column vh-100 gradlite">
+    <div id="app" className="d-flex flex-column gradlite vh-100">
       <NavBar />
       <div className="container flex-grow-1">
         <div className="mt-5">
